01c252a56fa164b498a9d9866aaabb6cc0f89052
CharlotteKluth <162616101+CharlotteKluth@users.noreply.github.com>
2024-05-16 20:26:18 +0200

    16.05.2024

---
+++ mygameswebsite/Ninjaparkour/public/scenes/Play.js
@@ -0,0 +1,55 @@
+import { Player } from "../objects/Player.js";
+
+export class Play extends Phaser.Scene {
+    constructor() {
+        super('play');
+        this.player = null;
+    }
+
+    create() {
+
+        //Erstelle Level
+        const map = this.make.tilemap({ key: "level1" });
+
+        //Erstelle Kachelsets/Tileset
+        const terrainCollideTileset = map.addTilesetImage("terrain_collide", "terrain_img");
+
+
+        //Erstelle Ebenen/Layer
+        const terrainLayer = map.createLayer("terrain", [terrainCollideTileset]);
+
+        //Erstelle Spieler
+        this.player = new Player(this, 200, 600, "Max", "Mask Dude");
+
+        //Kamera verfolgt Spieler
+        this.cameras.main.startFollow(this.player, true);
+        this.cameras.main.setZoom(1.4);
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+        //Erstelle Kollision mit den Blöcken
+        terrainLayer.setCollisionByProperty({ collide: true });
+        this.physics.add.collider(this.player, terrainLayer);
+
+        //Kill Bausteine
+        // const killTraps = map.filterTiles((tile) => tile.properties.kill === true);
+        terrainLayer.setCollisionByProperty({ kill: true });
+        this.physics.add.collider(this.player, terrainLayer, () => {
+            this.player.kill();
+        })
+
+        //Erstelle socket Verbindung
+        this.socket = io();
+
+        //Wenn Spieler sich verbindet
+        this.socket.on('connect', () => {
+            console.log('connected to server');
+            alert('connected to server');
+        })
+
+    }
+F
+    update() {
+
+    }
+
+}
